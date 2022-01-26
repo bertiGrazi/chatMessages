aea13f366d977acc61c16bf7cd9cc46c248cdd26
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-01-26 19:46:00 -0300

    15:08

--- chatMessages/chatMessages/Controllers/ProfileViewController.swift
+++ chatMessages/chatMessages/Controllers/ProfileViewController.swift
@@ -27,9 +27,39 @@ class ProfileViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.tableHeaderView = creatTableHeader()
         
         setuView()
         setupConstrains()
+    }
+    
+    func creatTableHeader() -> UIView? {
+//        guard let email = UserDefaults.standard.value(forKey: "email") as? String else { return nil }
+//
+//        let safeEmail = DatabaseManager.safeEmail(emailAdress: email)
+//        let filename = safeEmail + "_profile_picture.png"
+//        let path = "images/"+filename
+        
+       
+        let headerView = UIView(frame: CGRect(x: 0,
+                                            y: 0,
+                                            width: self.view.width,
+                                            height: 300))
+        headerView.backgroundColor = .link
+            
+        let imageView = UIImageView(frame: CGRect(x: (view.width-150)/2,
+                                                      y: 75,
+                                                      width: 150,
+                                                      height: 150))
+       
+        imageView.contentMode = .scaleToFill
+        imageView.backgroundColor = .white
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 3
+        imageView.layer.masksToBounds = true
+        
+        headerView.addSubview(imageView)
+        return headerView
     }
     
     fileprivate func setuView() {
